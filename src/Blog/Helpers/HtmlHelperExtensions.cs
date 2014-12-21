018430ec8dd42649dbb0edb790faef8a02ffb2d2
brz <borzoo.esmailloo@gmail.com>
2014-12-21 17:18:24 +0330

    added basic layout using bootstrap navbar

---
+++ src/Blog/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+namespace Blog.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string GetClassForNavbarItem(this HtmlHelper helper, string controller, string action)
+        {
+            var classToReturn = string.Empty;
+
+            if (controller == helper.ViewContext.RouteData.Values["controller"].ToString()
+                && action == helper.ViewContext.RouteData.Values["action"].ToString())
+            {
+                classToReturn = "active";
+            }
+
+            return classToReturn;
+        }
+    }
+}
---
+++ src/BlogTests/Helpers/HtmlHelperExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Xunit;
+using Blog.Helpers;
+using Xunit.Extensions;
+
+namespace BlogTests.Helpers
+{
+    public class HtmlHelperExtensionsTests
+    {
+        [Theory]
+        [InlineData("Home", "Index", "Home", "Index", "active")]
+        [InlineData("Home", "About", "Home", "Index", "")]
+        [InlineData("Account", "Index", "Home", "Index", "")]
+        [InlineData("Account", "Index", "Account", "Index", "active")]
+        public void GetClassForNavbarItem(string controller, string action, string currentController, string currentAction, string expectedClass)
+        {
+            Assert.Equal(expectedClass, GetHtmlHelperMock(currentController, currentAction).GetClassForNavbarItem(controller, action));
+        }
+
+        private HtmlHelper GetHtmlHelperMock(string controller, string action)
+        {
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", controller);
+            routeData.Values.Add("action", action);
+            var viewContextMock = new Mock<ViewContext>();
+            viewContextMock.Setup(x => x.RouteData).Returns(routeData);
+            return new HtmlHelper(viewContextMock.Object, new Mock<IViewDataContainer>().Object);
+        }
+    }
+}
