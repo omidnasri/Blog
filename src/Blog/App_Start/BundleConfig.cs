3c01fa1ad2a0cf4aa2205b7783e0226df36ca7f3
brz <borzoo.esmailloo@gmail.com>
2014-12-23 20:52:59 +0330

    added style and script bundles

---
+++ src/Blog/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Blog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap").Include("~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap-rtl").Include("~/Scripts/bootstrap-rtl.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JQuery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Styles/Bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Styles/Bootstrap-rtl").Include("~/Content/css/bootstrap-rtl.css"));
+        }
+    }
+}
--- src/Blog/Global.asax.cs
+++ src/Blog/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Blog
@@ -19,6 +20,7 @@ namespace Blog
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
