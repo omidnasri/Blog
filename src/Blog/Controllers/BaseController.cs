61bdc0f52d0e57a3f835b52bedb16c9192e2287b
brz <borzoo.esmailloo@gmail.com>
2014-12-23 19:45:58 +0330

    added basic globalization

---
+++ src/Blog/Controllers/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class BaseController : Controller
+    {
+        private Dictionary<string, Func<CultureInfo>> _cultureDic;
+
+        public BaseController()
+        {
+            _cultureDic = new Dictionary<string, Func<CultureInfo>>
+            {
+                { "fa", () => new CultureInfo("fa") },
+            };
+        }
+
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            SetCulture();
+            return base.BeginExecuteCore(callback, state);
+        }
+
+        /// <summary>
+        /// Sets the thread culture using request cookies, defaults to en-us
+        /// </summary>
+        public void SetCulture()
+        {
+            var cultureCookie = Request.Cookies["_culture"];
+            var culture = new CultureInfo("en-us");
+
+            if (cultureCookie != null && _cultureDic.ContainsKey(cultureCookie.Value))
+            {
+                culture = _cultureDic[cultureCookie.Value]();
+            }
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
