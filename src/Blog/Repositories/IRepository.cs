6205bcadf9f586df182082ffd38f47045635d5e8
brz <borzoo.esmailloo@gmail.com>
2015-01-12 23:57:20 +0330

    added base repository

---
+++ src/Blog/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Blog.Repositories
+{
+    internal interface IRepository<T>
+    {
+        bool Update(T entity);
+        bool Insert(T entity);
+        bool Delete(T entity);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetBy(Expression<Func<T, bool>> filter);
+    }
+}
